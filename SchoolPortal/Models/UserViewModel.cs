efeaf1100ef2465cb260486c02b3d38821bff6cb
Mathias Bergquist <berka28@hotmail.com>
2021-02-21 19:31:10 +0100

    Update 2

---
+++ SchoolPortal/Models/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPortal.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public string DisplayName => $"{FirstName} {LastName}";
+    }
+}
